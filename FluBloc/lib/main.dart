daf8cfa4c03bfd31340c747230e7a1d9b0e4e5c1
davidtran <davidtran641@gmail.com>
2020-06-17 17:20:48 +0800

    (FEAT) Add main.dart - change state

---
+++ FluBloc/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Incrementer(_incrementCounter),
+    );
+  }
+}
+
+class Incrementer extends StatefulWidget {
+  final Function increment;
+  Incrementer(this.increment);
+
+  @override
+  _IncrementerState createState() => _IncrementerState();
+}
+
+class _IncrementerState extends State<Incrementer> {
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: widget.increment,
+      tooltip: 'Increment',
+      child: Icon(Icons.add),
+    );
+  }
+}
